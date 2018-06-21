63498f9d1c95ef5ae2ae7bebef3ade4f54316159
Eugen Ganshorn <eugen.ganshorn@check24.de>
2018-06-21 22:55:30 +0200

    add swiftmailer bundle, templating, configure fos_user bundle

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
 ];
