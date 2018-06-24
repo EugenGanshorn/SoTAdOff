3bd798487d0345bf1313eb31541370bc76647a35
Eugen Ganshorn <eugen.ganshorn@check24.de>
2018-06-24 21:26:38 +0200

    add toggle functionality

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,11 +3,36 @@
 namespace App\Controller;
 
 use App\Repository\DeviceRepository;
+use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use GuzzleHttp\Client;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use TasmotaHttpClient\Request;
+use TasmotaHttpClient\Url;
 
+/**
+ * @Route("/")
+ */
 class IndexController extends Controller
 {
+    /**
+     * @var Client
+     */
+    protected $client;
+
+    /**
+     * @var Request
+     */
+    protected $request;
+
+    /**
+     * @var Url
+     */
+    protected $url;
+
     /**
      * @var DeviceRepository
      */
@@ -25,6 +50,33 @@ class IndexController extends Controller
     }
 
     /**
+     * @Route("/{id}", requirements={"id" = "\d+"}, name="toggle")
+     * @Method({"GET"})
+     * @param EntityManagerInterface $entityManager
+     * @param int                    $id
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function toggle(EntityManagerInterface $entityManager, int $id): \Symfony\Component\HttpFoundation\RedirectResponse
+    {
+        $device = $this->deviceRespository->find($id);
+
+        if (null !== $device) {
+            $this->request->getUrl()->setIpAddress($device->getIpAddress());
+
+            $this->request->Power(2);
+
+            $status = $this->request->Status(0);
+            $device->setPower($status['Status']['Power']);
+
+            $entityManager->persist($device);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('index');
+    }
+
+    /**
      * @required
      *
      * @param DeviceRepository $deviceRespository
@@ -34,6 +86,45 @@ class IndexController extends Controller
     public function setDeviceRespository(DeviceRepository $deviceRespository): IndexController
     {
         $this->deviceRespository = $deviceRespository;
+        return $this;
+    }
+
+    /**
+     * @required
+     *
+     * @param Client $client
+     *
+     * @return IndexController
+     */
+    public function setClient(Client $client): IndexController
+    {
+        $this->client = $client;
+        return $this;
+    }
+
+    /**
+     * @required
+     *
+     * @param Request $request
+     *
+     * @return IndexController
+     */
+    public function setRequest(Request $request): IndexController
+    {
+        $this->request = $request;
+        return $this;
+    }
+
+    /**
+     * @required
+     *
+     * @param Url $url
+     *
+     * @return IndexController
+     */
+    public function setUrl(Url $url): IndexController
+    {
+        $this->url = $url;
         return $this;
     }
 }
